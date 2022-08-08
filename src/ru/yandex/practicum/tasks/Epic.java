02554b774611ca4aadf13ae5b47b71fd98b6209b
Sergei <starodumovtmt@yandex.ru>
2022-08-08 08:12:09 +0500

    Style: Убрать пустую строку...

--- src/ru/yandex/practicum/tasks/Epic.java
+++ src/ru/yandex/practicum/tasks/Epic.java
@@ -8,7 +8,6 @@ public class Epic extends Task {
     public Epic(String taskName, String taskDescription, TaskStatus status) {
         super(taskName, taskDescription, status);
         subtasksIdForEpic = new ArrayList<>();
-
     }
 
     public ArrayList<Integer> getSubtaskIdForEpic() {
