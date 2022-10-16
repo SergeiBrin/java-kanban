758e65a62dfea01649d199959eaee1d70395e634
Sergei <starodumovtmt@yandex.ru>
2022-10-16 14:08:15 +0500

    fix: добавить проверку load объектов с save объектами

--- src/ru/yandex/practicum/managers/FileBackedTasksManager.java
+++ src/ru/yandex/practicum/managers/FileBackedTasksManager.java
@@ -1,13 +1,18 @@
 package ru.yandex.practicum.managers;
 
 import ru.yandex.practicum.exception.ManagerSaveException;
-import ru.yandex.practicum.tasks.*;
+import ru.yandex.practicum.tasks.Epic;
+import ru.yandex.practicum.tasks.Subtask;
+import ru.yandex.practicum.tasks.Task;
+import ru.yandex.practicum.tasks.enums.TaskStatus;
+import ru.yandex.practicum.tasks.formatter.TaskCsvFormatter;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -33,46 +38,103 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
         Path of = Path.of("resources/backed_file.csv");
 
         // Этап 1: сохранение в csv файл.
-        FileBackedTasksManager fileBackedTasksManager = new FileBackedTasksManager(of);
+        FileBackedTasksManager saveFileBackedTasksManager = new FileBackedTasksManager(of);
 
-        fileBackedTasksManager.createTask(task);
-        fileBackedTasksManager.createEpic(epic);
-        fileBackedTasksManager.createSubtask(epic, subtask);
-        fileBackedTasksManager.createSubtask(epic, subtask1);
+        saveFileBackedTasksManager.createTask(task);
+        saveFileBackedTasksManager.createEpic(epic);
+        saveFileBackedTasksManager.createSubtask(epic, subtask);
+        saveFileBackedTasksManager.createSubtask(epic, subtask1);
 
-        fileBackedTasksManager.getEpicById(2);
-        fileBackedTasksManager.getSubtaskById(4);
+        saveFileBackedTasksManager.getEpicById(2);
+        saveFileBackedTasksManager.getSubtaskById(4);
 
-        // fileBackedTasksManager.deleteSubtaskById(4);
-        // fileBackedTasksManager.deleteEpicById(2);
+        // saveFileBackedTasksManager.deleteSubtaskById(4);
+        // saveFileBackedTasksManager.deleteEpicById(2);
+        // saveFileBackedTasksManager.deleteTaskById(1);
 
-        fileBackedTasksManager.printTaskList();
-        fileBackedTasksManager.printEpicList();
-        fileBackedTasksManager.printSubtaskList();
+        boolean saveTaskList = saveFileBackedTasksManager.getTasksList().isEmpty();
+        boolean saveEpicList = saveFileBackedTasksManager.getEpicsList().isEmpty();
+        boolean saveSubtaskList = saveFileBackedTasksManager.getSubTasksList().isEmpty();
 
-        System.out.println();
-
-        List<Task> lists = fileBackedTasksManager.getHistory();
-        for (Task list : lists) {
-            System.out.println(list);
+        if (!(saveTaskList && saveEpicList && saveSubtaskList)) {
+            System.out.println("Список созданных задач:");
+            saveFileBackedTasksManager.printTaskList();
+            saveFileBackedTasksManager.printEpicList();
+            saveFileBackedTasksManager.printSubtaskList();
+            System.out.println();
         }
 
-
-
-        /* Этап 2: загрузка из csv файла.
-        FileBackedTasksManager newFileBackedTasksManager = FileBackedTasksManager.loadFromFile(of);
-
-        newFileBackedTasksManager.printTaskList();
-        newFileBackedTasksManager.printEpicList();
-        newFileBackedTasksManager.printSubtaskList();
-
-        System.out.println();
-
-        List<Task> lists = newFileBackedTasksManager.getHistory();
-        for (Task list : lists) {
-            System.out.println(list);
+        List<Task> savelistHistory = saveFileBackedTasksManager.getHistory();
+        if (!savelistHistory.isEmpty()) {
+            System.out.println("Список истории созданных задач:");
+            for (Task list : savelistHistory) {
+                System.out.println(list);
+            }
+            System.out.println();
         }
-        */
+
+        // Этап 2: загрузка из csv файла.
+        FileBackedTasksManager loadFileBackedTasksManager = FileBackedTasksManager.loadFromFile(of);
+
+        // Метод, который проверяет загруженные из файла задачи с оригинальными – на соответствие.
+        compareLoadAndSaveTasks(saveFileBackedTasksManager, loadFileBackedTasksManager);
+
+        boolean loadTaskList = loadFileBackedTasksManager.getTasksList().isEmpty();
+        boolean loadEpicList = loadFileBackedTasksManager.getEpicsList().isEmpty();
+        boolean loadSubtaskList = loadFileBackedTasksManager.getSubTasksList().isEmpty();
+
+        if (!(loadTaskList && loadEpicList && loadSubtaskList)) {
+            System.out.println("Список загруженных из файла задач:");
+            loadFileBackedTasksManager.printTaskList();
+            loadFileBackedTasksManager.printEpicList();
+            loadFileBackedTasksManager.printSubtaskList();
+            System.out.println();
+        }
+
+        List<Task> loadListHistory = loadFileBackedTasksManager.getHistory();
+        if (!loadListHistory.isEmpty()) {
+            System.out.println("Список истории загруженных из файла задач:");
+            for (Task list : loadListHistory) {
+                System.out.println(list);
+            }
+            System.out.println();
+        }
+    }
+
+    // Метод, который проверяет загруженные из файла задачи с оригинальными – на соответствие.
+    private static void compareLoadAndSaveTasks(
+            FileBackedTasksManager saveFileBackedTasksManager,
+            FileBackedTasksManager loadFileBackedTasksManager
+    ) {
+        // Вытягиваю все созданные задачи из Map и добавляю в лист.
+        List<Task> saveTasksList = new ArrayList<>();
+        saveTasksList.addAll(saveFileBackedTasksManager.getTasksList());
+        saveTasksList.addAll(saveFileBackedTasksManager.getEpicsList());
+        saveTasksList.addAll(saveFileBackedTasksManager.getSubTasksList());
+
+        // Вытягиваю все загруженные задачи из Map и добавляю в лист.
+        List<Task> loadTasksList = new ArrayList<>();
+        loadTasksList.addAll(loadFileBackedTasksManager.getTasksList());
+        loadTasksList.addAll(loadFileBackedTasksManager.getEpicsList());
+        loadTasksList.addAll(loadFileBackedTasksManager.getSubTasksList());
+
+        // Сравниваю загруженные из файла задачи с оригинальными.
+        for (Task loadTask : loadTasksList) {
+            if (!(saveTasksList.contains(loadTask))) {
+                System.out.println("\n!!!!! " + loadTask + "\nЭта задача, загруженная из файла, отличается от оригинала.");
+            }
+        }
+
+        // Аналогично для объектов истории
+        List<Task> saveHistoryTaskList = saveFileBackedTasksManager.getHistory();
+        List<Task> loadHistoryTaskList = loadFileBackedTasksManager.getHistory();
+
+        for (Task loadHistoryTask : loadHistoryTaskList) {
+            if (!(saveHistoryTaskList.contains(loadHistoryTask))) {
+                System.out.println("\n!!!!! " + loadHistoryTask + "\nЭта историческая задача, загруженная из файла, " +
+                        "отличается от оригинала.");
+            }
+        }
     }
 
     public void save() {
@@ -83,6 +145,9 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(path.toFile()))) {
             // Если все пусто, то return.
+            // Когда я выношу это условие из try, то программа не чистит файл, когда все мапы пусты.
+            // Это происходит, потому что мапы очищаются в первую очередь, а файл – только после открытия потока.
+            // Как очистить файл, не открывая поток, я чёт не знаю...
             boolean isNoTasks = (tasks.size() == 0) && (epics.size() == 0) && (subtasks.size() == 0);
             if (isNoTasks) {
                 return;
@@ -116,12 +181,12 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
             writer.write("\n");
             writer.flush();
 
-            // Добавяю id истории файл
+            // Добавляю id истории файл
             String historyId = TaskCsvFormatter.historyToString(getHistory());
             writer.write(historyId);
             writer.flush();
         } catch (IOException e) {
-            throw new ManagerSaveException();
+            throw new ManagerSaveException("Ошибка"); // Что ловит catch? Подумать над сообщением.
         }
     }
 
@@ -138,8 +203,9 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
         }
 
         // Если пусто, то дальше идти нет смысла.
+        // Я понял про табуляцию. Не было отступа самой строки, а я и не заметил.
         if (csvFile.isEmpty()) {
-        return fileBackedTasksManager;
+            return fileBackedTasksManager;
         }
 
         // Режем по \n и проходимся по массиву со второй строки, так как 1 строка - хедер.
@@ -266,6 +332,7 @@ public class FileBackedTasksManager extends InMemoryTaskManager {
     @Override
     public void deleteSubtaskById(int removeSubtask) {
         super.deleteSubtaskById(removeSubtask);
+
         save();
     }
 
